d4a891880fc623ecba016673dd65dc147210f091
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-21 17:48:04 -0300

    Adicionando menu aos joguinhos

--- Python 3 parte 1 - Introdução à nova versão da linguagem/jogos/adivinhacao.py
+++ Python 3 parte 1 - Introdução à nova versão da linguagem/jogos/adivinhacao.py
@@ -1,43 +1,49 @@
 import random
 
-print("Bem vindo ao jogo de adivinhação!!!")
 
-dificuldade = int(input(
-    "Selecione a sua dificuldade: \n 1-Fácil(0-50) \n 2-Médio(0-100) \n 3-Difícil(0-200) \n"))
+def jogar_adivinhacao():
 
-if(dificuldade == 1):
-    numero_secreto = random.randrange(1, 51)
-elif(dificuldade == 2):
-    numero_secreto = random.randrange(1, 101)
-else:
-    numero_secreto = random.randrange(1, 201)
+    print("Bem vindo ao jogo de adivinhação!!!")
 
-acertou = False
-pontuacao = 1000
+    dificuldade = int(input(
+        "Selecione a sua dificuldade: \n 1-Fácil(0-50) \n 2-Médio(0-100) \n 3-Difícil(0-200) \n"))
 
-while(acertou != True):
-    palpite = int(input("Digite o seu palpite do número secreto: "))
-
-    if(palpite == numero_secreto):
-        print("Parabéns, você acertou e fez {} pontos".format(pontuacao))
-        acertou = True
-    elif(palpite > numero_secreto):
-        print("O seu palpite foi MAIOR do que o número secreto!")
-        pontuacao -= palpite - numero_secreto
+    if(dificuldade == 1):
+        numero_secreto = random.randrange(1, 51)
+    elif(dificuldade == 2):
+        numero_secreto = random.randrange(1, 101)
     else:
-        print("O seu palpite foi MENOR do que o número secreto!")
-        pontuacao -= numero_secreto - palpite
+        numero_secreto = random.randrange(1, 201)
 
-#total_de_tentativas = 5
+    acertou = False
+    pontuacao = 1000
 
-# for rodada in range(1,total_de_tentativas +1):
-#     print("Rodada {} de {}".format(rodada, total_de_tentativas))
-#     palpite = int(input("Digite o seu palpite do número secreto: "))
+    while(acertou != True):
+        palpite = int(input("Digite o seu palpite do número secreto: "))
 
-#     if(palpite == numero_secreto):
-#         print("Parabéns, você acertou!!")
-#         break
-#     elif(palpite > numero_secreto):
-#         print("O seu palpite foi MAIOR do que o número secreto!")
-#     else:
-#         print("O seu palpite foi MENOR do que o número secreto!")
+        if(palpite == numero_secreto):
+            print("Parabéns, você acertou e fez {} pontos".format(pontuacao))
+            acertou = True
+        elif(palpite > numero_secreto):
+            print("O seu palpite foi MAIOR do que o número secreto!")
+            pontuacao -= palpite - numero_secreto
+        else:
+            print("O seu palpite foi MENOR do que o número secreto!")
+            pontuacao -= numero_secreto - palpite
+
+    #total_de_tentativas = 5
+
+    # for rodada in range(1,total_de_tentativas +1):
+    #     print("Rodada {} de {}".format(rodada, total_de_tentativas))
+    #     palpite = int(input("Digite o seu palpite do número secreto: "))
+
+    #     if(palpite == numero_secreto):
+    #         print("Parabéns, você acertou!!")
+    #         break
+    #     elif(palpite > numero_secreto):
+    #         print("O seu palpite foi MAIOR do que o número secreto!")
+    #     else:
+    #         print("O seu palpite foi MENOR do que o número secreto!")
+
+if(__name__ == "__main__"):
+    jogar_adivinhacao()
