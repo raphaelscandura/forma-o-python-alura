55b62a58f7700d0bfb117703175de3276d4c0e3b
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-21 15:00:04 -0300

    Melhorando o joguinho

--- Python 3 parte 1 - Introdução à nova versão da linguagem/jogos/adivinhacao.py
+++ Python 3 parte 1 - Introdução à nova versão da linguagem/jogos/adivinhacao.py
@@ -1,10 +1,18 @@
 print("Bem vindo ao jogo de adivinhação!!!")
 
 numero_secreto = 42
+total_de_tentativas = 5
+rodada = 0
 
-palpite = int(input("Digite o seu palpite do número secreto: "))
+while(rodada <= total_de_tentativas):
+    print("Rodada {} de {}".format(rodada, total_de_tentativas))
+    palpite = int(input("Digite o seu palpite do número secreto: "))
 
-if(palpite == numero_secreto):
-    print("Parabéns, você acertou!!")
-else:
-    print("Seu palpite está ERRADO! Tente novamente!")
+    if(palpite == numero_secreto):
+        print("Parabéns, você acertou!!")
+        break
+    elif(palpite > numero_secreto):
+        print("O seu palpite foi MAIOR do que o número secreto!")
+    else:
+        print("O seu palpite foi MENOR do que o número secreto!")
+    rodada += 1
