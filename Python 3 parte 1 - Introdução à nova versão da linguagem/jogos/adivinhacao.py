937d7e79e266f168adc94ddba104ee9ae764f99c
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-21 15:34:05 -0300

    Laço FOR e operadores booleanos

--- Python 3 parte 1 - Introdução à nova versão da linguagem/jogos/adivinhacao.py
+++ Python 3 parte 1 - Introdução à nova versão da linguagem/jogos/adivinhacao.py
@@ -2,11 +2,28 @@ print("Bem vindo ao jogo de adivinhação!!!")
 
 numero_secreto = 42
 total_de_tentativas = 5
-rodada = 0
+
+# for rodada in range(1,total_de_tentativas +1):
+#     print("Rodada {} de {}".format(rodada, total_de_tentativas))
+#     palpite = int(input("Digite o seu palpite do número secreto: "))
+
+#     if(palpite == numero_secreto):
+#         print("Parabéns, você acertou!!")
+#         break
+#     elif(palpite > numero_secreto):
+#         print("O seu palpite foi MAIOR do que o número secreto!")
+#     else:
+#         print("O seu palpite foi MENOR do que o número secreto!")
+
+rodada = 1
 
 while(rodada <= total_de_tentativas):
     print("Rodada {} de {}".format(rodada, total_de_tentativas))
-    palpite = int(input("Digite o seu palpite do número secreto: "))
+    palpite = int(input("Digite o seu palpite do número secreto (entre 0 e 100): "))
+
+    if(palpite < 1 or palpite > 100):
+        print("Palpite inválido! Por favor, digite um número de 0 a 100")
+        continue
 
     if(palpite == numero_secreto):
         print("Parabéns, você acertou!!")
