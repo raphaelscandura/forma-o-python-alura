fa4d2298eb985bf49be64cf8fcfcfdc0ef757985
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-21 13:37:29 -0300

    First commit, começando os estudos

---
+++ Python 3 parte 1 - Introdução à nova versão da linguagem/testes-iniciais/app.py
@@ -0,0 +1,11 @@
+# Entendendo sintaxe, variáveis dinâmicas etc.
+
+pais = "brasil"
+print(type(pais))
+titulos = 5
+print(type(titulos))
+print(pais, "tem", titulos, "titulos de copa do mundo")
+pais = 5
+print(type(pais))
+titulos = "brasil"
+print(type(titulos))
