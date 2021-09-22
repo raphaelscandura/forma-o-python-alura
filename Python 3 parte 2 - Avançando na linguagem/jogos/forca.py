84bf10624c46e5bcfa96bf6152236e306446d380
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-22 16:17:39 -0300

    Jogo da forca completo, curso finalizado

--- Python 3 parte 2 - Avançando na linguagem/jogos/forca.py
+++ Python 3 parte 2 - Avançando na linguagem/jogos/forca.py
@@ -4,6 +4,37 @@ import random
 def jogar_forca():
     print("Bem vido ao jogo da Forca!")
 
+    palavra_secreta = definir_palavra_secreta()
+
+    acertou = False
+    tentativas = 6
+    letras_da_palavra = ["_" for letra in palavra_secreta]
+
+    while(tentativas != 0 and not acertou):
+        print(letras_da_palavra)
+        palpite_letra = input(
+            "Digite uma letra que você acha que tem na palavra secreta: ")
+        palpite_letra_tratado = palpite_letra.strip().upper()
+
+        if(palpite_letra_tratado in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(palpite_letra_tratado == letra):
+                    letras_da_palavra[index] = letra
+                index += 1
+        else:   
+            tentativas -= 1
+        desenha_forca(tentativas)
+        acertou = "_" not in letras_da_palavra
+
+    if(acertou):
+        print("Você ganhou! Parabéns")
+    else:
+        print("Suas tentativas acabaram, você perdeu!")
+
+    print("Fim do jogo")
+
+def definir_palavra_secreta():
     arquivo = open("palavras.txt", "w")
     arquivo.write("banana\n")
     arquivo.write("abacaxi\n")
@@ -25,33 +56,52 @@ def jogar_forca():
     numero = random.randrange(0, len(lista))
 
     palavra_secreta = lista[numero].upper()
-    acertou = False
-    tentativas = 6
-    letras_da_palavra = ["_" for letra in palavra_secreta]
 
-    while(tentativas != 0 and not acertou):
-        print(letras_da_palavra)
-        palpite_letra = input(
-            "Digite uma letra que você acha que tem na palavra secreta: ")
-        palpite_letra_tratado = palpite_letra.strip().upper()
+    return palavra_secreta
 
-        if(palpite_letra_tratado in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if(palpite_letra_tratado == letra):
-                    letras_da_palavra[index] = letra
-                index += 1
-        else:
-            print("Palpite errado! A palavra secreta não possui essa letra")
-            tentativas -= 1
-        acertou = "_" not in letras_da_palavra
+def desenha_forca(tentativas):
+    print("  _______     ")
+    print(" |/      |    ")
 
-    if(acertou):
-        print("Você ganhou! Parabéns")
-    else:
-        print("Suas tentativas acabaram, você perdeu!")
+    if(tentativas == 5):
+        print(" |     (º-º)  ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
 
-    print("Fim do jogo")
+    if(tentativas == 4):
+        print(" |     (º-º)  ")
+        print(" |       |    ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(tentativas == 3):
+        print(" |     (º-º)  ")
+        print(" |      \|    ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(tentativas == 2):
+        print(" |     (º-º)  ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(tentativas == 1):
+        print(" |     (º-º)  ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (tentativas == 0):
+        print(" |     (º-º)  ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 if(__name__ == "__main__"):
