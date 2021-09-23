f5c4121dc801242a153ed3cd8690bb5f1736bc40
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-23 15:48:14 -0300

    Mudando getters and setters para properties

--- Python 3 - Introdução a Orientação a objetos/Conta.py
+++ Python 3 - Introdução a Orientação a objetos/Conta.py
@@ -16,27 +16,35 @@ class Conta:
         self.sacar(valor)
         contaDestino.depositar(valor)
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
-    
-    def get_numero(self):
+
+    @property
+    def numero(self):
         return self.__numero
-    
-    def get_titular(self):
+
+    @property
+    def titular(self):
         return self.__titular
-    
-    def get_agencia(self):
+
+    @property
+    def agencia(self):
         return self.__agencia
     
-    def set_saldo(self, saldo):
+    @saldo.setter
+    def saldo(self, saldo):
         self.__saldo = saldo
     
-    def set_agencia(self, agencia):
+    @agencia.setter
+    def agencia(self, agencia):
         self.__agencia = agencia
     
-    def set_numero(self, numero):
+    @numero.setter
+    def numero(self, numero):
         self.__numero = numero
     
-    def set_titular(self, titular):
+    @titular.setter
+    def titular(self, titular):
         self.__titular = titular
     
---
+++ Python 3 - Introdução a Orientação a objetos/Testes.py
@@ -0,0 +1,4 @@
+from Cliente import Cliente
+
+cliente = Cliente("Pablo")
+print(cliente.nome)
