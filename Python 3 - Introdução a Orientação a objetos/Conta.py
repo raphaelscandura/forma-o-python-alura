5748f140899a4bc0159399b17abe6a542721a506
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-23 15:15:57 -0300

    Mudando visibilidade para private

--- Python 3 - Introdução a Orientação a objetos/Conta.py
+++ Python 3 - Introdução a Orientação a objetos/Conta.py
@@ -1,32 +1,32 @@
 class Conta:
 
     def __init__(self, agencia, numero, titular, saldo):
-        self.agencia = agencia
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
+        self.__agencia = agencia
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
 
     def get_saldo(self):
-        return self.saldo
+        return self.__saldo
     
     def get_numero(self):
-        return self.numero
+        return self.__numero
     
     def get_titular(self):
-        return self.get_titular
+        return self.__titular
     
     def get_agencia(self):
-        return self.agencia
+        return self.__agencia
     
     def set_saldo(self, saldo):
-        self.saldo = saldo
+        self.__saldo = saldo
     
     def set_agencia(self, agencia):
-        self.agencia = agencia
+        self.__agencia = agencia
     
     def set_numero(self, numero):
-        self.numero = numero
+        self.__numero = numero
     
     def set_titular(self, titular):
-        self.titular = titular
+        self.__titular = titular
     
