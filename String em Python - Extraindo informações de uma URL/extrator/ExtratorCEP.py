3a17481def2db58a5743776c5417246007a8bd73
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-25 15:17:15 -0300

    Refatorando regex com quantificadores

--- String em Python - Extraindo informações de uma URL/extrator/ExtratorCEP.py
+++ String em Python - Extraindo informações de uma URL/extrator/ExtratorCEP.py
@@ -5,7 +5,7 @@ class ExtratorCEP:
         self._endereco = endereco
 
     def extrai_cep(self):
-        pattern = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+        pattern = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
         busca = pattern.search(self._endereco)
         if busca:
             cep = busca.group()
