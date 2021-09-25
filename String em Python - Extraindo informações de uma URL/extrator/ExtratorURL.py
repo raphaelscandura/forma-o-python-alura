ee3471d5cec88aa88f1a2d86af4437dc243908ec
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-25 15:42:50 -0300

    Curso finalizado

--- String em Python - Extraindo informações de uma URL/extrator/ExtratorURL.py
+++ String em Python - Extraindo informações de uma URL/extrator/ExtratorURL.py
@@ -1,3 +1,5 @@
+import re
+
 class ExtratorURL:
     def __init__(self, url):
         self._url = self.sanitiza_url(url)
@@ -18,8 +20,12 @@ class ExtratorURL:
             return ""
     
     def _validaURL(self):
-        if not self.url:
+        if not self._url:
             raise ValueError("A URL está vazia!")
+        padrao_url = re.compile('(http(s)?://)?(www.)?bytebank.com(.br)?/cambio')
+        match = padrao_url.match(self._url)
+        if not match:
+            raise ValueError("A URL não é válida!")
         
     def get_url_base(self):
         indice_interrogacao = self._url.find('?')
@@ -38,3 +44,12 @@ class ExtratorURL:
         else:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
+
+    def __len__(self):
+        return len(self._url)
+
+    def __str__(self):
+        return self._url + "\n" +"Parâmetros: " + self.get_url_parametros() + "\n" + "URL Base: " + self.get_url_base()
+
+    def __eq__(self, outro_url):
+        return self._url == outro_url.url
--- String em Python - Extraindo informações de uma URL/extrator/main.py
+++ String em Python - Extraindo informações de uma URL/extrator/main.py
@@ -1,7 +1,8 @@
 from ExtratorURL import ExtratorURL
 from ExtratorCEP import ExtratorCEP
 
-url = ExtratorURL("https://bytebank.com/busca?moedaOrigem=real&quantidade=1000&moedaDestino=dolar")
+url = ExtratorURL("https://bytebank.com/cambio?moedaOrigem=real&quantidade=1000&moedaDestino=dolar")
+url2 = ExtratorURL("https://bytebank.com/cambio?moedaOrigem=real&quantidade=1000&moedaDestino=dolar")
 
 print(url.get_url_base())
 print(url.get_url_parametros())
@@ -9,3 +10,7 @@ print(url.get_valor_parametro("quantidade"))
 
 cep = ExtratorCEP("R. Goitacazes, 71 - CentroSão Caetano do Sul - SP, 09510-300")
 print(cep.extrai_cep())
+
+print(url)
+print(url2 == url)
+print(url is url2)
