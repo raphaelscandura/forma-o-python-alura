5f388a78ca063c6a6412f105986f85dc4eb32acc
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-25 13:54:23 -0300

    Explorando fatiamento de strings

--- String em Python - Extraindo informações de uma URL/extrator/main.py
+++ String em Python - Extraindo informações de uma URL/extrator/main.py
@@ -1,10 +1,19 @@
 url = "bytebank.com/cambio?moedaOrigem=real"
 print(url)
 
-url_base = url[0:19]
+index_interrogacao = url.find('?')
+
+url_base = url[:index_interrogacao]
 print(url_base)
 
-url_parametros = url[20:36]
+url_parametros = url[index_interrogacao + 1:]
 print(url_parametros)
 
 print(url)
+
+parametro_origem = 'moedaOrigem'
+indice_primeiro_parametro = url.find(parametro_origem)
+indice_moeda_origem = indice_primeiro_parametro + len(parametro_origem) + 1
+primeiro_parametro = url[indice_moeda_origem:]
+
+print(primeiro_parametro)
