64a38746fa3e77e8fdb13d251c2a5cac421cb675
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-09-28 13:01:28 -0300

    defaultdict

--- Python Collections parte 2 - Conjuntos e dicionários/dict.py
+++ Python Collections parte 2 - Conjuntos e dicionários/dict.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 texto = "bom dia senhoras e senhores no dia de hoje teremos bom senhores e boa senhoras"
 print(set(texto.split()))
 
@@ -15,4 +17,18 @@ print(quantidade_de_aparicoes["bom"])
 print(quantidade_de_aparicoes.get("e"))
 
 quantidade_de_aparicoes = dict(Bom = 2, dia = 1)
+print(quantidade_de_aparicoes)
+quantidade_de_aparicoes["teste"] = 5
+print(quantidade_de_aparicoes)
+quantidade_de_aparicoes["teste"] = 6
+print(quantidade_de_aparicoes)
+del quantidade_de_aparicoes["teste"]
+print(quantidade_de_aparicoes)
+
+quantidade_de_aparicoes = defaultdict(int)
+
+for palavra in texto.split():
+    aparicoes = quantidade_de_aparicoes[palavra]
+    quantidade_de_aparicoes[palavra] = aparicoes + 1
+
 print(quantidade_de_aparicoes)
