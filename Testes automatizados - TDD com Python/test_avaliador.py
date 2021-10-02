6148f99ae1678a0233269232d105e7c55d25ab86
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-10-02 11:10:10 -0300

    Melhorando testes com setUp()

--- Testes automatizados - TDD com Python/test_avaliador.py
+++ Testes automatizados - TDD com Python/test_avaliador.py
@@ -4,88 +4,64 @@ from dominio import Usuario, Avaliador, Lance, Leilao
 
 class TestAvaliador(TestCase):
 
+    def setUp(self):
+        self.pedro = Usuario("Pedro")
+        self.gabriel = Usuario("Gabriel")
+
+        self.lance_do_pedro = Lance(self.pedro, 150.0)
+        self.lance_do_gabriel = Lance(self.gabriel, 100.0)
+
+        self.leilao = Leilao("Televisão")
+
     def test_retorna_o_menor_e_o_maior_lance_quando_os_lances_forem_dados_em_ordem_crescente(self):
-        pedro = Usuario("Pedro")
+        self.leilao.adicionar_lance(self.lance_do_gabriel)
+        self.leilao.adicionar_lance(self.lance_do_pedro)
 
-        gabriel = Usuario("Gabriel")
-
-        lance_do_pedro = Lance(pedro, 150.0)
-        lance_do_gabriel = Lance(gabriel, 100.0)
-
-        leilao = Leilao("Televisão")
-
-        leilao.adicionar_lance(lance_do_pedro)
-        leilao.adicionar_lance(lance_do_gabriel)
-
-        avaliador = Avaliador()
-        avaliador.avalia(leilao)
-
+        self.avaliador = Avaliador()
+        self.avaliador.avalia(self.leilao)
+        
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.avaliador.maior_lance)
 
     def test_retorna_o_menor_e_o_maior_lance_quando_os_lances_forem_dados_em_ordem_decrescente(self):
-        pedro = Usuario("Pedro")
+        self.leilao.adicionar_lance(self.lance_do_pedro)
+        self.leilao.adicionar_lance(self.lance_do_gabriel)
 
-        gabriel = Usuario("Gabriel")
-
-        lance_do_pedro = Lance(pedro, 150.0)
-        lance_do_gabriel = Lance(gabriel, 100.0)
-
-        leilao = Leilao("Televisão")
-
-        leilao.adicionar_lance(lance_do_gabriel)
-        leilao.adicionar_lance(lance_do_pedro)
-        
-
-        avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        self.avaliador = Avaliador()
+        self.avaliador.avalia(self.leilao)
 
         menor_valor_esperado = 100.0
         maior_valor_esperado = 150.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.avaliador.maior_lance)
     
     def test_retorna_o_mesmo_valor_para_maior_e_menor_lance_quando_o_leilao_so_tiver_um_lance(self):
-        pedro = Usuario("Pedro")
+        self.leilao.adicionar_lance(self.lance_do_pedro)
 
-        lance_do_pedro = Lance(pedro, 150.0)
-
-        leilao = Leilao("Televisão")
-
-        leilao.adicionar_lance(lance_do_pedro)        
-
-        avaliador = Avaliador()
-        avaliador.avalia(leilao)
+        self.avaliador = Avaliador()
+        self.avaliador.avalia(self.leilao)
 
         valor_lance_esperado = 150.0
 
-        self.assertEqual(valor_lance_esperado, avaliador.menor_lance)
-        self.assertEqual(valor_lance_esperado, avaliador.maior_lance)
+        self.assertEqual(valor_lance_esperado, self.avaliador.menor_lance)
+        self.assertEqual(valor_lance_esperado, self.avaliador.maior_lance)
     
     def test_retorna_o_menor_e_o_maior_lance_para_mais_de_dois_lances(self):
-        pedro = Usuario("Pedro")
         lola = Usuario("Lola")
-        gabriel = Usuario("Gabriel")
-
-        lance_do_pedro = Lance(pedro, 150.0)
         lance_da_lola = Lance(lola, 20.0)
-        lance_do_gabriel = Lance(gabriel, 100.0)
+        self.leilao.adicionar_lance(self.lance_do_gabriel)
+        self.leilao.adicionar_lance(self.lance_do_pedro)
+        self.leilao.adicionar_lance(lance_da_lola)
 
-        leilao = Leilao("Televisão")
+        self.avaliador = Avaliador()
+        self.avaliador.avalia(self.leilao)
 
-        leilao.adicionar_lance(lance_do_pedro)
-        leilao.adicionar_lance(lance_da_lola)
-        leilao.adicionar_lance(lance_do_gabriel)
-
-        avaliador = Avaliador()
-        avaliador.avalia(leilao)
-
-        menor_valor_esperado = 100.0
+        menor_valor_esperado = 20.0
         maior_valor_esperado = 150.0
 
-        self.assertEqual(menor_valor_esperado, avaliador.menor_lance)
-        self.assertEqual(maior_valor_esperado, avaliador.maior_lance)
+        self.assertEqual(menor_valor_esperado, self.avaliador.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.avaliador.maior_lance)
