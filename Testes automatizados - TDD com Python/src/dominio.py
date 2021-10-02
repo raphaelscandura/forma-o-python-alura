ab25e088aa9633801dff6bbcfe1753a83ec5d3ad
Raphael Scandura da Cunha <raphaelscandura@hotmail.com>
2021-10-02 15:55:18 -0300

    Refatorando o código

--- Testes automatizados - TDD com Python/src/dominio.py
+++ Testes automatizados - TDD com Python/src/dominio.py
@@ -1,5 +1,3 @@
-import sys
-
 class Usuario:
 
     def __init__(self, nome, carteira):
@@ -9,19 +7,22 @@ class Usuario:
     @property
     def nome(self):
         return self.__nome
-    
+
     @property
     def carteira(self):
         return self.__carteira
-    
+
     def da_lance(self, leilao, valor_lance):
-        if(valor_lance > self.carteira):
-            raise ValueError("O usuário não possui dinheiro suficiente na carteira para bancar um lance deste valor")
+        if(self._tem_dinheiro(valor_lance)):
+            raise ValueError(
+                "O usuário não possui dinheiro suficiente na carteira para bancar um lance deste valor")
         else:
-            lance_valido = Lance(self,valor_lance)
+            lance_valido = Lance(self, valor_lance)
             leilao.adicionar_lance(lance_valido)
             return lance_valido
-
+    
+    def _tem_dinheiro(self, valor_lance):
+        return valor_lance > self.carteira
 
 class Lance:
 
@@ -29,35 +30,47 @@ class Lance:
         self.usuario = usuario
         self.valor = valor
 
-
 class Leilao:
 
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-        self.maior_lance = sys.float_info.min
-        self.menor_lance = sys.float_info.max
+        self.maior_lance = 0.0
+        self.menor_lance = 0.0
 
     @property
     def lances(self):
         return self.__lances[:]
-    
+
     def adicionar_lance(self, lance):
-        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.lances[-1].valor:
-            if(self.maior_lance < lance.valor):
-                self.maior_lance = lance.valor
-            if(self.menor_lance > lance.valor):
+        if self._lance_valido(lance):
+            if self._lista_de_lances_vazia():
                 self.menor_lance = lance.valor
+            self.maior_lance = lance.valor
             self.__lances.append(lance)
         else:
             raise ValueError("Erro: Lance inválido")
 
     def imprimir_todos_os_lances(self):
         for lance in self.__lances:
-            print(f'O usuário {lance.usuario.nome} deu um lance de {lance.valor}')
+            print(
+                f'O usuário {lance.usuario.nome} deu um lance de {lance.valor}')
 
     def imprimir_maior_e_menor_lances(self):
-        print(f'O maior lance foi de {self.maior_lance} e o menor lance foi de {self.menor_lance}')
+        print(
+            f'O maior lance foi de {self.maior_lance} e o menor lance foi de {self.menor_lance}')
 
     def ultimo_lance(self):
-        return self.__lances[-1]    
+        return self.__lances[-1]
+
+    def _lance_valido(self, lance):
+        return self._lista_de_lances_vazia() or self._usuarios_diferentes(lance) and self._lance_maior_que_anterior(lance)
+
+    def _lista_de_lances_vazia(self):
+        return not self.__lances
+
+    def _usuarios_diferentes(self, lance):
+        return self.ultimo_lance().usuario != lance.usuario
+
+    def _lance_maior_que_anterior(self, lance):
+        return lance.valor > self.ultimo_lance().valor
